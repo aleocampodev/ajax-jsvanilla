b4ca7107284f7a4bfa7de4fac2e4bf17f62b51ce
ocampoaleja <omaira.527@hotmail.com>
2020-05-25 16:09:17 -0500

    feature: added XMLHttpRequest

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const message  = document.getElementById('message');
+
+message.addEventListener('click', bringData);
+
+function bringData(){
+   const httpasync= new XMLHttpRequest();
+
+   httpAsync.open('GET','archivo.txt',true);
+
+   httpAsync.send();
+
+   httpAsync.onreadystatechange = funcion(){
+      if(this.readyState == 4 && this.status == 200){
+
+      }
+   }
+
+
+}
+
